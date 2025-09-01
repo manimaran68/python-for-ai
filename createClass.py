f1d874776c426cff861a7c23af7d6f29ecdf2cd3
manimaran68 <vklmaran68@gmail.com>
2025-09-01 08:21:35 +0530

    class assignment 1

---
+++ createClass.py
@@ -0,0 +1,66 @@
+class subfieldsInAI():
+    def subfields():
+        print("Sub-fields in AI are:")
+        print("Machine Learning")
+        print("Neural Networks")
+        print("Vision")
+        print("Robotics")
+        print("Speech Processing")
+        print("Natural Language Processing")
+
+    def oddEven():
+        num=int(input("Enter a number: "))
+        if((num%2)==1):
+            print(f"{num} is odd number")
+        else:
+            print(f"{num} is Even number")
+            return message
+
+    def marriage_eligibility():
+        gender = input("Your Gender (Male/Female): ")
+        age = int(input("Your Age: "))
+    
+        if (gender == "Male"):
+            if (age >= 21):
+                print("ELIGIBLE")
+            else:
+                print("NOT ELIGIBLE")
+        elif (gender == "Female"):
+            if (age >= 18):
+                print("ELIGIBLE")
+            else:
+                print("NOT ELIGIBLE")
+        else:
+            print("Invalid gender entered")
+
+    def calculate_percentage():
+        Subject1 = int(input("Subject 1: "))
+        Subject2 = int(input("Subject 2: "))
+        Subject3 = int(input("Subject 3: "))
+        Subject4 = int(input("Subject 4: "))
+        Subject5 = int(input("Subject 5: "))
+    
+        Total = Subject1 + Subject2 + Subject3 + Subject4 + Subject5
+        Percentage = (Total / 500) * 100
+    
+        print("Total :", Total)
+        print("Percentage :", Percentage)
+
+    def triangle_calculations():
+        Height = int(input("Height: "))
+        Breadth = int(input("Breadth: "))
+        Area = (Height * Breadth) / 2
+        print("Area formula: (Height*Breadth)/2")
+        print("Area of Triangle:", Area)
+    
+        Height1 = int(input("Height1: "))
+        Height2 = int(input("Height2: "))
+        Breadth = int(input("Breadth: "))
+        Perimeter = Height1 + Height2 + Breadth
+        print("Perimeter formula: Height1+Height2+Breadth")
+        print("Perimeter of Triangle:", Perimeter)
+
+            
+
+                
+    
